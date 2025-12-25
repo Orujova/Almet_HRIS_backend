699d2636c1d651dc9ef93c861837bb911531d5b3
Orujova Narmin <nerminorucova2004@gmail.com>
2025-12-25 10:07:05 +0400

    update

--- almet_hris_backend/almet_hris_backend/celery.py
+++ almet_hris_backend/almet_hris_backend/celery.py
@@ -29,7 +29,7 @@ app.conf.beat_schedule = {
     'send-daily-celebrations': {
     'task': 'api.tasks.send_daily_celebration_notifications',
     # 'schedule': crontab(minute='*/2'),# # 🧪 TEST: Every 2 minutes
-    'schedule': crontab(hour=10, minute=0),
+    'schedule': crontab(hour=9, minute=0),
 },
 }
 
